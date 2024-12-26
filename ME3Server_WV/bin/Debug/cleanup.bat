93a36361448ff48bb59157f9e367b9175f524789
Kadrim <8304331+kadrim@users.noreply.github.com>
2024-12-26 11:51:01 +0100

    fix cleanup

--- ME3Server_WV/bin/Debug/cleanup.bat
+++ ME3Server_WV/bin/Debug/cleanup.bat
@@ -2,14 +2,11 @@ del ME3PlayerDataEditor.pdb
 del ME3PlayerDataEditor.xml
 del PLAYER_FILES.txt
 del ME3Server_WV.pdb
-del ME3Server_WV.vshost.exe
-del ME3Server_WV.vshost.exe.config
-del ME3Server_WV.vshost.exe.manifest
+del Syndicate2012Server.exe
+del Syndicate2012Server.exe.config
+del Syndicate2012Server.manifest
+del Syndicate2012Server.pdb
 del zlib.net.pdb
 cd logs
 del *.* /F /Q
 cd ..
-cd player
-del *.* /F /Q
-cd gaw
-del *.* /F /Q
