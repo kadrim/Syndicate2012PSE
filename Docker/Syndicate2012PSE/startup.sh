b797187b452fa1e3ec30588269dcedf7392d6d6c
kadrim <8304331+kadrim@users.noreply.github.com>
2024-12-29 19:17:45 +0100

    update gui docker for linux to be able to run dotnet 8 apps

--- Docker/Syndicate2012PSE/startup.sh
+++ Docker/Syndicate2012PSE/startup.sh
@@ -3,7 +3,7 @@
 if [ "$HEADLESS" == "true" ]
     then
         echo "Running in headless mode"
-        xvfb-run wine Syndicate2012Server.exe
+        xvfb-run -a wine Syndicate2012Server.exe
     else
         echo "Running in GUI mode"
         wine Syndicate2012Server.exe
