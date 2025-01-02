9af4110123eb0ced61352f5f60d2bc86c51106bc
kadrim <8304331+kadrim@users.noreply.github.com>
2025-01-02 01:50:23 +0100

    fix sudo call

--- Docker/Syndicate2012PSE/startup.sh
+++ Docker/Syndicate2012PSE/startup.sh
@@ -6,8 +6,8 @@ chmod -R 777 player
 if [ "$HEADLESS" == "true" ]
     then
         echo "Running in headless mode"
-        sudo -u appuser umask 0000 && xvfb-run -a wine Syndicate2012Server.exe
+        sudo -u appuser bash -c 'umask 0000 && xvfb-run -a wine Syndicate2012Server.exe'
     else
         echo "Running in GUI mode"
-        sudo -u appuser umask 0000 && wine Syndicate2012Server.exe
+        sudo -u appuser bash -c 'umask 0000 && wine Syndicate2012Server.exe'
 fi
